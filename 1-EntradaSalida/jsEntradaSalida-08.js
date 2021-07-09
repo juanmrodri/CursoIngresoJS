54f4a86c9b53243aa683c2be6a4e40787d91b12c
juanmrodri <juanmrodriguez1989@gmail.com>
2021-07-09 18:01:01 -0300

    correccion ejercicio 8

--- 1-EntradaSalida/jsEntradaSalida-08.js
+++ 1-EntradaSalida/jsEntradaSalida-08.js
@@ -3,19 +3,16 @@ Debemos lograr tomar Los numeros por ID ,
 transformarlos a enteros (parseInt),realizar la operación correcta y 
 mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
-function SacarResto()
-{
-	
-	var numeroUno;
-	var numeroDos;
-	var resto;
-	var resultado;
-	numeroUno=3;
-	numeroDos=66;
+function SacarResto() {
+  var numeroUno;
+  var numeroDos;
+  var resto;
+  numeroUno = 3;
+  numeroDos = 66;
 
-	resultado=numeroUno%numeroDos;
+  resto = numeroUno % numeroDos;
 
-	console.log("El resultado es: "+resultado);
+  console.log("El resto es: " + resto);
 
-	alert("El resto es: "+resto)
+  alert("El resto es: " + resto);
 }
