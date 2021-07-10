16d9eeaa038edbcb74f79022305e98f784c91a8d
juanmrodri <juanmrodriguez1989@gmail.com>
2021-07-10 11:25:40 -0300

    tp3

--- 8-TPs/03-jsFerretePinturas.js
+++ 8-TPs/03-jsFerretePinturas.js
@@ -2,25 +2,32 @@
 A.	Al ingresar una temperatura en Fahrenheit debemos mostrar la temperatura en Centígrados con un mensaje concatenado (ej.: " 32 Fahrenheit son 0 centígrados").
 B.	Al ingresar una temperatura en Centígrados debemos mostrar la temperatura en Fahrenheit (ej.: "0  centígrados son 32 Fahrenheit ").
 */
-function FahrenheitCentigrados () 
-{
-	var centigrados;
-    var fahrenheit;
-    var valorIngresado;
-    var mensaje;
+function FahrenheitCentigrados() {
+  var centigrados;
+  var valorIngresado;
+  var mensaje;
 
-    centigrados=document.getElementById('txtIdTemperatura').value;
-    centigrados=parseInt(centigrados);
-    valorIngresado=centigrados;
-    centigrados=centigrados/32;
+  centigrados = document.getElementById("txtIdTemperatura").value;
+  centigrados = parseInt(centigrados);
+  valorIngresado = centigrados;
+  centigrados = (centigrados-32)*5/9;
 
-    mensaje=centigrados+""
+  mensaje = valorIngresado + " Fahrenheit son " + centigrados + " centigrados";
 
-    alert(mensaje);
-
+  alert(mensaje);
 }
 
-function CentigradosFahrenheit () 
-{
-	
+function CentigradosFahrenheit() {
+  var fahrenheit;
+  var valorIngresado;
+  var mensaje;
+
+  fahrenheit = document.getElementById("txtIdTemperatura").value;
+  fahrenheit = parseInt(fahrenheit);
+  valorIngresado = fahrenheit;
+  fahrenheit = (fahrenheit*9/5)+32;
+
+  mensaje = valorIngresado + " centigrados son " + fahrenheit + " Fahrenheit";
+
+  alert(mensaje);
 }
