278632da06d5b5f30e48c13b1fe6af1c2b9b2db2
juanmrodri <juanmrodriguez1989@gmail.com>
2021-07-11 11:31:13 -0300

    ejercicio 07 - E/S

--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -7,21 +7,94 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	alert("ok");	
+	var numeroUno;
+	var numeroDos;
+	var sumar;
+	var mensaje;
+	
+	numeroUno=document.getElementById('txtIdNumeroUno').value;
+	numeroUno=parseInt(numeroUno);
+
+	numeroDos=document.getElementById('txtIdNumeroDos').value;
+	numeroDos=parseInt(numeroDos);
+
+	sumar=numeroUno+numeroDos;
+
+
+	mensaje="La suma es: "+sumar;
+	
+
+	alert(mensaje);
+
+
 }
 
 function restar()
 {
+	var numeroUno;
+	var numeroDos;
+	var restar;
+	var mensaje;
+	
+	numeroUno=document.getElementById('txtIdNumeroUno').value;
+	numeroUno=parseInt(numeroUno);
+
+	numeroDos=document.getElementById('txtIdNumeroDos').value;
+	numeroDos=parseInt(numeroDos);
+
+	restar=numeroUno-numeroDos;
+
+
+	mensaje="La resta es: "+restar;
+	
+
+	alert(mensaje);
+
 	
 }
 
 function multiplicar()
 { 
+	var numeroUno;
+	var numeroDos;
+	var multiplicar;
+	var mensaje;
 	
+	numeroUno=document.getElementById('txtIdNumeroUno').value;
+	numeroUno=parseInt(numeroUno);
+
+	numeroDos=document.getElementById('txtIdNumeroDos').value;
+	numeroDos=parseInt(numeroDos);
+
+	multiplicar=numeroUno*numeroDos;
+
+
+	mensaje="La multiplicación es: "+multiplicar;
+	
+
+	alert(mensaje);
+
 }
 
 function dividir()
 {
+	var numeroUno;
+	var numeroDos;
+	var division;
+	var mensaje;
 	
+	numeroUno=document.getElementById('txtIdNumeroUno').value;
+	numeroUno=parseInt(numeroUno);
+
+	numeroDos=document.getElementById('txtIdNumeroDos').value;
+	numeroDos=parseInt(numeroDos);
+
+	division=numeroUno/numeroDos;
+
+
+	mensaje="La división es: "+division;
+	
+
+	alert(mensaje);
 }
 
