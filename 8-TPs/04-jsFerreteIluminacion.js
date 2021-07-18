8292c1314cf57c59ec998682dbe848f7d9d659a1
juanmrodri <juanmrodriguez1989@gmail.com>
2021-07-18 13:40:09 -0300

    tp 04 ferrete, codigo mas limpio

--- 8-TPs/04-jsFerreteIluminacion.js
+++ 8-TPs/04-jsFerreteIluminacion.js
@@ -1,6 +1,6 @@
 /*4.	
 autor: Juan Manuel Rodriguez
-ejercicio: Ferrete iluminación 04
+ejercicio: Ferrete iluminación 04 - corregido
 Para el departamento de iluminación:
 Tomando en cuenta que todas las lámparas están en oferta al mismo precio de $35 pesos final.
 A.	Si compra 6 o más  lamparitas bajo consumo tiene un descuento del 50%. 
@@ -11,133 +11,117 @@ E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
  ”Usted pago X de IIBB.”, siendo X el impuesto que se pagó. 
 
  */
-function CalcularPrecio () 
-{
-    // lampara unidad
-    var precioLampara;
-    // lamparas seleccionadas
-    var cantidadLamparas;
-    // precio de la cantidad de lamparas seleccionadas
-    var lamparasTotal;
-    // descuentos
-     var descuento;
-    // marca lampara
-    var marcaLampara;
-    // marca descuento
-    var precioDescuento;
-    //precio final con descuento
-    var precioFinal;
-    // ingreso bruto
-    var ingresoBruto;
-    // precio con IIBB
-    var precioIibb;
-    // ultimo precio
-    var ultimoPrecio;
-    // mensaje final
-    var mensaje;
-
-    // por ahora valen 35
-    precioLampara=35;
-
-    // ingreso bruto valor
-
-    ingresoBruto=10;
-
-    // cantidad de lamparas
-    cantidadLamparas=document.getElementById("txtIdCantidad").value;
-    cantidadLamparas=parseInt(cantidadLamparas);
-
-    //guardo la marca
-    marcaLampara=document.getElementById("Marca").value;
-
-    
-    if(cantidadLamparas>5)
-    { 
-      // situación A
-      descuento=50;
-      lamparasTotal=cantidadLamparas*precioLampara;
-      precioDescuento=lamparasTotal*descuento/100;
-      precioFinal=lamparasTotal-precioDescuento;
-    } else 
-        {   
-            if(cantidadLamparas==3){
-                    if(marcaLampara=="ArgentinaLuz") {
-                        // situación D op. 1
-                        descuento=15;
-                        lamparasTotal=cantidadLamparas*precioLampara;
-                        precioDescuento=lamparasTotal*descuento/100;
-                        precioFinal=lamparasTotal-precioDescuento;
-                    } else 
-                        {
-                            if(marcaLampara=="FelipeLamparas") {
-                                // situación D op. 2
-                                descuento=10;
-                                lamparasTotal=cantidadLamparas*precioLampara;
-                                precioDescuento=lamparasTotal*descuento/100;
-                                precioFinal=lamparasTotal-precioDescuento;
-                            } else 
-                                {
-                                    // situación D op. 3
-                                    descuento=5;
-                                    lamparasTotal=cantidadLamparas*precioLampara;
-                                    precioDescuento=lamparasTotal*descuento/100;
-                                    precioFinal=lamparasTotal-precioDescuento;
-                                }
-                        }
-            } 
-            else 
-                {
-                    if(cantidadLamparas==4)
-                                    {
-                                        if(marcaLampara=="ArgentinaLuz" || marcaLampara=="FelipeLamparas")
-                                        {   // situación C
-                                            descuento=25;
-                                            lamparasTotal=cantidadLamparas*precioLampara;
-                                            precioDescuento=lamparasTotal*descuento/100;
-                                            precioFinal=lamparasTotal-precioDescuento;
-                                        } else
-                                            {   // situación C
-                                                descuento=20;
-                                                lamparasTotal=cantidadLamparas*precioLampara;
-                                                precioDescuento=lamparasTotal*descuento/100;
-                                                precioFinal=lamparasTotal-precioDescuento;
-                                            }
-                                    } else 
-                                        {
-                                            if(cantidadLamparas==5 && marcaLampara=="ArgentinaLuz")
-                                                {   // situación B op. 1
-                                                    descuento=40;
-                                                    lamparasTotal=cantidadLamparas*precioLampara;
-                                                    precioDescuento=lamparasTotal*descuento/100;
-                                                    precioFinal=lamparasTotal-precioDescuento;
-                                                } else
-                                                    {   // situación B op. 2
-                                                        descuento=30;
-                                                        lamparasTotal=cantidadLamparas*precioLampara;
-                                                        precioDescuento=lamparasTotal*descuento/100;
-                                                        precioFinal=lamparasTotal-precioDescuento;
-                                                    }
-                                        }
-                }
-        }
-
-
-
-    // precio con IIBB
-    if(precioFinal>119)
-    {
-        precioIibb=precioFinal*ingresoBruto/100;
-        ultimoPrecio=precioFinal+precioIibb;
-        mensaje="IIBB Usted pago: "+precioIibb+" siendo el precio final: "+ultimoPrecio;
-    } else
-        {
-            mensaje="El precio con descuento es: "+precioFinal;
-        }
-
-    
-
-    // campo para mostrar resultado
-    document.getElementById("txtIdprecioDescuento").value=mensaje;
-
-
-}
+ function CalcularPrecio () 
+ {
+     // lampara unidad
+     var precioLampara;
+     // lamparas seleccionadas
+     var cantidadLamparas;
+     // precio de la cantidad de lamparas seleccionadas
+     var lamparasTotal;
+     // descuentos
+      var descuento;
+     // marca lampara
+     var marcaLampara;
+     // marca descuento
+     var precioDescuento;
+     //precio final con descuento
+     var precioFinal;
+     // ingreso bruto
+     var ingresoBruto;
+     // precio con IIBB
+     var precioIibb;
+     // ultimo precio
+     var ultimoPrecio;
+     // mensaje final
+     var mensaje;
+ 
+     // por ahora valen 35
+     precioLampara=35;
+ 
+     // ingreso bruto valor
+ 
+     ingresoBruto=10;
+ 
+     // cantidad de lamparas
+     cantidadLamparas=document.getElementById("txtIdCantidad").value;
+     cantidadLamparas=parseInt(cantidadLamparas);
+ 
+     //guardo la marca
+     marcaLampara=document.getElementById("Marca").value;
+ 
+     
+     if(cantidadLamparas>5)
+     { 
+       // situación A
+       descuento=50;
+       
+     } else 
+         {   
+             if(cantidadLamparas==3){
+                     if(marcaLampara=="ArgentinaLuz") {
+                         // situación D op. 1
+                         descuento=15;
+                     } else 
+                         {
+                             if(marcaLampara=="FelipeLamparas") {
+                                 // situación D op. 2
+                                 descuento=10;
+                             } else 
+                                 {
+                                     // situación D op. 3
+                                     descuento=5;
+                                 }
+                         }
+             } 
+             else 
+                 {
+                     if(cantidadLamparas==4)
+                                     {
+                                         if(marcaLampara=="ArgentinaLuz" || marcaLampara=="FelipeLamparas")
+                                         {   // situación C
+                                             descuento=25;
+                                         } else
+                                             {   // situación C
+                                                 descuento=20;
+                                             }
+                                     } else 
+                                         {
+                                             if(cantidadLamparas==5 && marcaLampara=="ArgentinaLuz")
+                                                 {   // situación B op. 1
+                                                     descuento=40;
+                                                 } else
+                                                     {   // situación B op. 2
+                                                         descuento=30;
+                                                     }
+                                         }
+                 }
+         }
+ 
+     
+     // con todos los datos hago la operación
+     lamparasTotal=cantidadLamparas*precioLampara;
+     precioDescuento=lamparasTotal*descuento/100;
+     precioFinal=lamparasTotal-precioDescuento;
+ 
+     // precio con IIBB
+     if(precioFinal>119)
+     {
+         precioIibb=precioFinal*ingresoBruto/100;
+         ultimoPrecio=precioFinal+precioIibb;
+         mensaje="IIBB Usted pago: "+precioIibb+" siendo el precio final: "+ultimoPrecio;
+     } else
+         {
+             mensaje="El precio con descuento es: "+precioFinal;
+         }
+ 
+     
+ 
+     // campo para mostrar resultado
+     document.getElementById("txtIdprecioDescuento").value=mensaje;
+ 
+ 
+ }
+ 
+ 
+ 
