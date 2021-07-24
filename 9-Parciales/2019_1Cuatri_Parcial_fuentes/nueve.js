a09e8fe7f27b074c95c64b7a34795602bf61eb18
juanRodriguez <juanmrodriguez1989@gmail.com>
2021-07-24 19:30:43 -0300

    principio de parcial 2019 ej. 9

--- 9-Parciales/2019_1Cuatri_Parcial_fuentes/nueve.js
+++ 9-Parciales/2019_1Cuatri_Parcial_fuentes/nueve.js
@@ -5,5 +5,19 @@ ejercicio: Parcial 2019 - 09
 
 function mostrar()
 {
+    var pais;
+    var cantidadHabitantes;
+    var temperaturaMinima;
+    var temperaturaMaxima;
+    var consulta;
+
+    consulta=true;
+
+    while(consulta)
+    {
+        
+        
+        consulta=confirm("Desea continuar?");
+    }
 
 }
